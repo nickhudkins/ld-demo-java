6dad607e230f3eb7294a553fc615b4da3387d5d7
Lev Lazinskiy <lev@launchdarkly.com>
2018-08-07 15:44:16 -0700

    add Logging to FeatureProvider

--- src/main/java/org/levlaz/FeatureProvider.java
+++ src/main/java/org/levlaz/FeatureProvider.java
@@ -2,15 +2,28 @@ package org.levlaz;
 
 import com.launchdarkly.client.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Singleton Enum
  */
 public enum FeatureProvider {
     INSTANCE;
 
+    private static final Logger logger = LoggerFactory.getLogger(FeatureProvider.class.getName());
     private LDClient ldClient = new LDClient(System.getenv("LD_SDK_KEY"));
     
     public LDClient getInstance() {
+        checkEnv();
         return ldClient;
+    }
+
+    public void checkEnv() {
+        if (System.getenv("LD_SDK_KEY") == null) {
+            logger.warn("LD_SDK_KEY is not set as an ENV VAR, Feature Flags will fallback");
+        } else {
+            logger.info("LD_SDK_KEY starts with: " + System.getenv("LD_SDK_KEY").substring(0, 10));
+        }
     }
 }
